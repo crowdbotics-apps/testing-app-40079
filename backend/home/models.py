bf401444fd1a066a354cde5409f8949c136370a5
crowdbotics-apps <team@crowdbotics.com>
2023-04-21 14:07:20 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -11,12 +11,12 @@ class Recipe(models.Model):
 class AttendEvent(models.Model):
     'Generated Model'
     user = models.ForeignKey("users.User",on_delete=models.CASCADE,related_name="attendevent_user",)
-    event = models.ForeignKey("home.Event",on_delete=models.CASCADE,null=True,blank=True,related_name="attendevent_event",)
+    event = models.ForeignKey("home.Event",on_delete=models.CASCADE,related_name="attendevent_event",)
 class Event(models.Model):
     'Generated Model'
     name = models.CharField(max_length=256,)
     city = models.CharField(max_length=256,)
-    datatime = models.DateTimeField()
     created_at = models.DateTimeField()
     image = models.CharField(max_length=256,)
     info = models.TextField()
+    datetime = models.IntegerField(null=True,blank=True,)
