031c177a7be9f717e2207d4c4108e95929757f1c
crowdbotics-apps <team@crowdbotics.com>
2023-04-21 08:38:35 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+class Recipe(models.Model):
+    'Generated Model'
+    title = models.CharField(max_length=256,)
+    intruction = models.TextField()
+    prep_time = models.IntegerField()
+    cook_time = models.IntegerField()
+    rating = models.IntegerField()
+    image = models.CharField(max_length=256,)
